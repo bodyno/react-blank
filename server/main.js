a0c8da48d909deb520b7331c64b174a156028682
bodyno <az8321550@gmail.com>
2016-06-16 14:31:45 +0800

    Update many

--- server/main.js
+++ server/main.js
@@ -1,27 +1,33 @@
 import koa from 'koa'
-import convent from 'koa-convert'
+import convert from 'koa-convert'
+import serve from 'koa-static'
 import config from '../config'
 import webpack from 'webpack'
 import webpackConfig from '../config/webpack.config'
-import serve from 'koa-static'
 import historyApiFallback from 'koa-connect-history-api-fallback'
-import webpackDevMiddleware from './middleware/webpack-dev'
-import webpackHMRMiddleware from './middleware/webpack-hmr'
+import { devMiddleware, hotMiddleware } from 'koa-webpack-middleware'
 
 const app = new koa()
 const compiler = webpack(webpackConfig)
 const {__DEV__, src, dist} = config
 
-app.use(convent(historyApiFallback({
+app.use(convert(historyApiFallback({
   verbose: false
 })))
 
 if(__DEV__){
-  app.use(webpackDevMiddleware(compiler))
-  app.use(webpackHMRMiddleware(compiler))
-  app.use(convent(serve(src)))
+  app.use(devMiddleware(compiler), {
+    contentBase: '/src',
+    hot: true,
+    stats: {
+      chunks : false,
+      chunkModules : false,
+      colors : true
+    }
+  })
+  app.use(hotMiddleware(compiler))
 }else{
-  app.use(convent(serve(dist)))
+  app.use(convert(serve(dist)))
 }
 
 export default app
